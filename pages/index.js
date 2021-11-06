875cddf4d6070e200e0451dde4c7113a26aeea39
Ashok Kumar A M <ashokkumar.am@gmail.com>
2021-11-06 12:33:48 +0530

    first page

--- pages/index.js
+++ pages/index.js
@@ -10,39 +10,18 @@ export default function Home()
       </Head>
 
 
-      <div className="container p-32">
-        <label htmlFor="price" className="block text-sm font-medium text-gray-700">
-          Price
-        </label>
-        <div className="relative mt-1 rounded-md shadow-sm">
-          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <span className="text-gray-500 sm:text-sm">$</span>
-          </div>
-          <input
-            type="text"
-            name="price"
-            id="price"
-            className="block w-full pr-12 border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 pl-7 sm:text-sm"
-            placeholder="0.00"
-          />
-          <div className="absolute inset-y-0 right-0 flex items-center">
-            <label htmlFor="currency" className="sr-only">
-              Currency
-            </label>
-            <select
-              id="currency"
-              name="currency"
-              className="h-full py-0 pl-2 text-gray-500 bg-transparent border-transparent rounded-md focus:ring-indigo-500 focus:border-indigo-500 pr-7 sm:text-sm"
-            >
-              <option>USD</option>
-              <option>CAD</option>
-              <option>EUR</option>
-            </select>
-          </div>
-        </div>
+      <div className="bg-gray-900 min-h-screen flex items-center justify-center">
+        <h1 className="text-9xl text-white font-bold mx-auto max-w-6xl">
+          <span className="text-transparent bg-gradient-to-r bg-clip-text from-blue-600 to-green-600">BLACKBIRD
+          </span>
+        </h1>
       </div>
-
-
+      <div className="bg-gray-900 min-h-screen flex items-center justify-center">
+        <h1 className="text-9xl text-white font-bold mx-auto max-w-6xl">
+          <span className="text-transparent bg-gradient-to-l bg-clip-text from-red-600 to-green-600">BOILERPLATE
+          </span>
+        </h1>
+      </div>
 
     </div>
   )
